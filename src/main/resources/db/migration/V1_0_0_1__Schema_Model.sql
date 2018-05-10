a962d60a57e497b959dd12e7cf5ba6e1d7340fb3
Tri Mulyanto <trimulyanto.tm@gmail.com>
2018-05-11 00:23:29 +0700

    setting up password for superadmin

--- src/main/resources/db/migration/V1_0_0_1__Schema_Model.sql
+++ src/main/resources/db/migration/V1_0_0_1__Schema_Model.sql
@@ -3,7 +3,7 @@ VALUES('b8fffbcd-5466-11e8-ba0c-40f02fd895f6', 'SUPERADMIN', 'SUPERMAN');
 
 INSERT INTO clns_user
 (user_id, created_by, created_date, description, email, is_employee, employee_no, fullname, ip_address, is_new_user, last_login, password, password_expired, password_failed, status, updated_by, updated_date, user_expired, username)
-VALUES('4bad559e-5467-11e8-ba0c-40f02fd895f6', NULL, NULL, 'Description user', 'superadmin@clinicnesia.com', 1, '545454545', 'superadmin', NULL, 0, NULL, '$2a$10$GMcuhhGAwdcAFX90gSKbVOP.LlGi6RZxJb5kGSKY3vlh/tcfU3RO6', NULL, 0, 1, NULL, NULL, '2018-06-15 00:00:00.000', 'superadmin');
+VALUES('4bad559e-5467-11e8-ba0c-40f02fd895f6', NULL, NULL, 'Description user', 'superadmin@clinicnesia.com', 1, '545454545', 'superadmin', NULL, 0, NULL, '$2a$10$NIP38Xozm0zftQD.tob0NundGGLREWIdfCL5rMd.aX43DsdrDp6EO', NULL, 0, 1, NULL, NULL, '2018-06-15 00:00:00.000', 'superadmin');
 
 INSERT INTO clns_users_roles
 (user_id, role_id)
